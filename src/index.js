c5309ecf2706690f34d088a62345534bc11daf5e
Que <hoang.newking@gmail.com>
2020-08-20 22:12:09 +0700

    August 20, referencing JS variables in JSX

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+
 // Create React component
 const App = () => {
-    return <div>Hi there! i just changed something! i really excited to learn React</div>
+    const buttonText = {text: 'click me'}
+    const styleButton = {
+        backgroundColor: 'red',
+        color: 'white',
+        marginLeft: '3px'
+    }
+    let labelText = "Enter Name:"
+    return (
+    <div>
+            <label className="label" for="name">{labelText}</label>
+            <input id="name" type="text" />
+            <button style={styleButton}>
+                {buttonText.text}
+            </button>
+        </div>
+    )
 }
 
 // Take the react component and show it on the screen
